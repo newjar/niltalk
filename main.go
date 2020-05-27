758b211c75a2aaa56e1a3530907b5906ed98fc6e
yobelchris <yobelchris@gmail.com>
2020-05-27 14:23:02 +0700

    remove access to html pages

--- main.go
+++ main.go
@@ -243,7 +243,7 @@ func main() {
 		AllowCredentials: false,
 	}))
 
-	r.Get("/", wrap(handleIndex, app, 0))
+	//r.Get("/", wrap(handleIndex, app, 0))
 	r.Get("/ws/{roomID}", wrap(handleWS, app, hasAuth|hasRoom))
 
 	// API.
@@ -252,10 +252,10 @@ func main() {
 	r.Post("/api/rooms", wrap(handleCreateRoom, app, 0))
 
 	// Views.
-	r.Get("/r/{roomID}", wrap(handleRoomPage, app, hasAuth|hasRoom))
-	r.Get("/static/*", func(w http.ResponseWriter, r *http.Request) {
-		app.fs.FileServer().ServeHTTP(w, r)
-	})
+	//r.Get("/r/{roomID}", wrap(handleRoomPage, app, hasAuth|hasRoom))
+	//r.Get("/static/*", func(w http.ResponseWriter, r *http.Request) {
+	//	app.fs.FileServer().ServeHTTP(w, r)
+	//})
 
 	// Start the app.
 	srv := &http.Server{
